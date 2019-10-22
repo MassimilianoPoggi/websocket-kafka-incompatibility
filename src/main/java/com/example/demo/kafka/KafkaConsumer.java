b0ccf766ad23936feccd72e5341c56e7d3af66c1
Massimiliano Poggi <ma.poggi@reply.it>
2019-10-22 17:58:51 +0200

    Updated showcase: added correct binding

--- src/main/java/com/example/demo/kafka/KafkaConsumer.java
+++ src/main/java/com/example/demo/kafka/KafkaConsumer.java
@@ -2,8 +2,8 @@ package com.example.demo.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.binder.kafka.streams.annotations.KafkaStreamsProcessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +13,18 @@ import org.apache.kafka.streams.kstream.KStream;
  * KafkaConsumer
  */
 @Component
-@EnableBinding(KafkaStreamsProcessor.class)
+@EnableBinding(KafkaConsumer.ConsumerBinding.class)
 public class KafkaConsumer {
+    interface ConsumerBinding {
+        @Input("my-kafka-topic")
+        KStream<?, ?> input();
+    }
+
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
     @StreamListener("my-kafka-topic")
-    public void processMessage(KStream<String, String> input) {
+    public void process(KStream<String, String> input) {
         input.foreach((k, v) -> simpMessagingTemplate.convertAndSend("my-websocket-topic", v));
     }
 }
